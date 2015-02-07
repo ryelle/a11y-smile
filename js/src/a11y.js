7e3e3f3eb70ae675a65622d8b8d21861c64e9152
ryelle <kelly.dwan@gmail.com>
2015-02-07 17:08:47 -0500

    Remove secret, tokens have been regenerated.

--- js/src/a11y.js
+++ js/src/a11y.js
@@ -3,21 +3,6 @@ window.Smile = window.Smile || {};
 (function($, undefined){
 	Smile.a11y = {};
 
-	var auth = {
-		//
-		// Update with your auth tokens.
-		//
-		consumerKey : "d86aA6e9luwCkEBPQ8RsBw",
-		consumerSecret : "1qgY4NxisuKgE3-Sh3cYYaAPNHA",
-		accessToken : "XidIrU_2lh1X5O4u469jSLkjQzzRAWlZ",
-		// This example is a proof of concept, for how to use the Yelp v2 API with javascript.
-		// You wouldn't actually want to expose your access token secret like this in a real application.
-		accessTokenSecret : "WQMd4LhHdC3DHuWN2fZ01budG2A",
-		serviceProvider : {
-			signatureMethod : "HMAC-SHA1"
-		}
-	};
-
 	Smile.a11y.check = function( event, data ){
 		var element = document.getElementById( data.service + '-' + data.id );
 
